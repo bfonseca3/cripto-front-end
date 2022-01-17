b7ecd716240a4c6e0c4a7e4eda04c59de06a4fc3
Higor Allan <higor.allan21@gmail.com>
2022-01-17 13:02:40 -0300

    features/test-middleware

--- src/pages/_middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from "next/server";
-
-export function middleware(req: NextRequest) {
-  if (req.url !== `http://localhost:3000/login`) {
-    if (!req.cookies["cripto.auth"]) {
-      return Response.redirect("/login");
-    }
-  }
-}
